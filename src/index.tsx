574ed4429eb1587285d5b0ad310cde60f9ca1d42
Kevin Samosir <bintangkevin29@gmail.com>
2020-09-09 18:09:42 +0700

    added gh scripts

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={"/graph-and-memoized-table"}>
       <App />
     </BrowserRouter>
   </Provider>,
